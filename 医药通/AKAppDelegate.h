1b986553e9e156e0826a7dff59092fcfa7a6cba7
凯_SKK <q254522834@kai-SKKtekiMac-mini.local>
2013-03-26 10:02:14 +0800

    Initial Commit

---
+++ 医药通/AKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKAppDelegate.h
+//  医药通
+//
+//  Created by 凯_SKK on 13-3-26.
+//  Copyright (c) 2013年 山东乐世安通通信技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 医药通/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  医药通
+//
+//  Created by 凯_SKK on 13-3-26.
+//  Copyright (c) 2013年 山东乐世安通通信技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AKAppDelegate class]));
+    }
+}
